c2ef7c3f4a9498a24d6925ee42a16a5e0736213a
Fley27 <fleyvick@outlook.com>
2023-09-08 19:04:16 -0400

    k

---
+++ includes/section-nat-course.php
@@ -0,0 +1,85 @@
+<div class = "course-_">
+    <div class = "course--container">
+        <div class = "course--text">
+            <div class = "course--title">
+                Naturalization Course
+            </div>
+            <div class = "course--description">
+                As the immigration landscape continues to evolve 
+                and clients seek comprehensive support, 
+                the Education Department will enable us 
+                to provide holistic services that encompass 
+                both legal representation and guidance on 
+                education opportunities for immigrants.
+            </div>
+            <a href = "/wordpress/why-academia-de-suenos/" class = "course--btn">
+                Enroll Now!
+            </a>
+        </div>
+        <img class = "why-us-img" src="<?php echo get_template_directory_uri(); ?>/assets/images/puzzle.webp" alt="puzzle Academia De Sueños" loading="lazy">
+        <div class="body-course-content">
+            <div class="tab-course">
+                <a href="#about" class="tab-course-item">About</a>
+                <a href="#outcomes" class="tab-course-item active">Outcomes</a>
+                <a href="#curriculum" class="tab-course-item">Curriculum</a>
+            </div>
+            <div id = "about" class="course-section">
+                <div class="c-s-title">    
+                    What you'll learn              
+                </div>
+                <div class="c-s-body">
+                    <div class="c-s-b-item">
+                        <i class="fa-solid fa-check"></i>
+                        <span>Gain the skills required for an entry-level career as an iOS developer.</span>
+                    </div>
+                    <div class="c-s-b-item">
+                        <i class="fa-solid fa-check"></i>
+                        <span>Gain the skills required for an entry-level career as an iOS developer.</span>
+                    </div>
+                    <div class="c-s-b-item">
+                        <i class="fa-solid fa-check"></i>
+                        <span>Gain the skills required for an entry-level career as an iOS developer.</span>
+                    </div>
+                    <div class="c-s-b-item">
+                        <i class="fa-solid fa-check"></i>
+                        <span>Gain the skills required for an entry-level career as an iOS developer.</span>
+                    </div>
+                </div>
+            </div>
+            <div id = "outcomes" class="course-section">
+                <div class="c-s-title">    
+                    Prepare you to past your citizenship test          
+                </div>
+                <div class="c-s-body">
+                    <ul class="c-s-body-ul">
+                        <li class="c-s-body-li">Receive professional-level training from Meta</li>
+                        <li class="c-s-body-li">Demonstrate your proficiency in portfolio-ready projects</li>
+                        <li class="c-s-body-li">Receive career guidance with actionable steps toward landing a job</li>
+                        <li class="c-s-body-li">Receive professional-level training from Meta</li>
+                        <li class="c-s-body-li">Qualify for in-demand job titles: iOS Developer, Mobile Applications Developer, Mobile Developer</li>
+                    </ul>
+                </div>
+            </div>
+            <div id = "curriculum" class="course-section">
+                <div class="c-s-title">Naturalization Course Curriculum</div>
+                <div class="c-s-body">
+                    <span>
+                        Have you ever wanted to build outstanding mobile apps like Facebook and Instagram?
+                    </span>
+                    <span>
+                        This Professional Certificate will teach you how to build applications 
+                        for iOS devices and start a new career as an iOS Developer. By the end 
+                        of this program, you’ll be able to create and run a mobile app powered 
+                        by iOS operating systems.
+                    </span>
+                    <span>
+                        Once you complete the program, you’ll get access to the Meta Career 
+                        Programs Job Board—a job search platform that connects you with 200+ 
+                        employers who have committed to sourcing talent through Meta’s certificate 
+                        programs, as well as career support resources to help you with your job search.
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- templates/template-nat-course.php
+++ templates/template-nat-course.php
@@ -9,6 +9,9 @@
     get_header("nat-course");
 ?>
 
+<?php
+    get_template_part("includes/section", "nat-course");
+?>
 
 <?php
     get_footer();
