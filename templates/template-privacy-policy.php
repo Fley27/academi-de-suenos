bd32a50867639a53480d0061961c26bc37ab128d
Fley27 <fleyvick@outlook.com>
2023-11-17 10:02:50 -0400

    added privacy policy page

---
+++ templates/template-privacy-policy.php
@@ -0,0 +1,163 @@
+<?php
+    /* 
+        Template Name: Privacy Policy
+    */
+?>
+
+<?php get_header("success"); ?>
+
+<div class="privacy-policy">
+    <div class="privacy-content">
+        <div class="privacy-headline">Privacy Policy</div>
+        <br/>
+        <br>
+        <p>
+            <div class="privacy-title">What information do we collect?</div>
+            <div class="privacy-description">
+                <p>We collect information from you when you register on our site or fill out a form.</p>
+                <p>When registering on this site, as appropriate, you may be asked to enter your: name, e-mail address, phone number or other information. You may, however, visit our site anonymously.</p>
+                <p>Google, as a third-party vendor, uses cookies to serve ads on this site. Google’s use of the DART cookie enables it to serve ads to your users based on their visit to your sites and other sites on the Internet. Users may opt-out of the use of the DART cookie by visiting the Google ad and content network privacy policy.</p>
+            </div>
+        </p>
+        <br/>
+        <br/>
+        <p>
+            <div class="privacy-title">
+                What do we use your information for?
+            </div>
+            <div class="privacy-description">
+                <p>
+                    Any of the information we collect from you may be used in one of the following ways:
+                </p>
+                <ul>
+                    <li>To personalize your experience (your information helps us to better respond to your individual needs)</li>
+                    <li>To improve our website (we continually strive to improve our website offerings based on the information and feedback we receive from you)</li>
+                    <li>To improve customer service (your information helps us to more effectively respond to your customer service requests and support needs)</li>
+                </ul>
+                <p>
+                    <strong>
+                        How do we protect your information?
+                    </strong>
+                </p>
+                <p>
+                    We implement a variety of security measures to maintain the safety of your personal information when you access your personal information.
+                </p>
+                <p>
+                    We offer the use of a secure server. All supplied sensitive/credit information is transmitted via Secure Socket Layer (SSL) technology and then encrypted into our Database to be only accessed by those authorized with special access rights to our systems, and are required to keep the information confidential.
+                </p>
+                <p>
+                    After a transaction, your private information (credit cards, social security numbers, financials, etc.) will not be kept on file for more than 60 days.
+                </p>
+            </div>
+        </p>
+        <br/>
+        <br/>
+        <p>
+            <div class="privacy-title">
+                Do we use cookies?
+            </div>
+            <div class="privacy-description">
+                Yes (Cookies are small files that a site or its service provider transfers to your computers hard drive through your Web browser (if you allow) that enables the sites or service providers systems to recognize your browser and capture and remember certain information.erstand and save your preferences for future visits.
+            </div>
+        </p> 
+        <br/>
+        <br/>
+        <p>
+            <div class="privacy-title">
+                Do we disclose any information to outside parties?
+            </div>
+            <div class="privacy-description">
+                We do not sell, trade, or otherwise transfer to outside parties your personally identifiable information. This does not include trusted third parties who assist us in operating our website, conducting our business, or servicing you, so long as those parties agree to keep this information confidential. We may also release your information when we believe release is appropriate to comply with the law, enforce our site policies, or protect ours or others rights, property, or safety. However, non-personally identifiable visitor information may be provided to other parties for marketing, advertising, or other uses. 
+            </div>
+        </p>       
+        <br/>
+        <br/>
+        <p>
+            <div class="privacy-title">
+                Third-party links
+            </div>
+            <div class="privacy-description">
+                Occasionally, at our discretion, we may include or offer third-party products or services on our website. These third party sites have separate and independent privacy policies. We, therefore, have no responsibility or liability for the content and activities of these linked sites. Nonetheless, we seek to protect the integrity of our site and welcome any feedback about these sites.
+            </div>
+        </p>
+        <br/>
+        <br/>
+        <p>
+            <div class="privacy-title">
+                California Online Privacy Protection Act Compliance
+            </div>
+            <div class="privacy-description">
+                <p>
+                    Because we value your privacy we have taken the necessary precautions to be in compliance with the California Online Privacy Protection Act. We therefore will not distribute your personal information to outside parties without your consent.
+                </p>
+                <p>
+                    As part of the California Online Privacy Protection Act, all users of our site may make any changes to their information at any time by logging into their control panel and going to the ‘Edit Profile’ page.
+                </p>
+            </div>
+        </p>
+        <br/>
+        <br/>
+        <p>
+            <div class="privacy-title">
+                Children’s Online Privacy Protection Act Compliance
+            </div>
+            <div class="privacy-description">
+                <p>
+                    We are in compliance with the requirements of COPPA (Children’s Online Privacy Protection Act), we do not collect any information from anyone under 13 years of age. Our website, products and services are all directed to people who are at least 13 years old or older
+                </p>
+            </div>
+        </p>
+        <br/>
+        <br/>
+        <p>
+            <div class="privacy-title">
+                Online Privacy Policy Only
+            </div>
+            <div class="privacy-description">
+                <p>
+                    This online privacy policy applies only to information collected through our website and not to information collected offline.
+                </p>
+            </div>
+        </p>
+        <br/>
+        <br/>
+        <p>
+            <div class="privacy-title">
+                Terms and Conditions
+            </div>
+            <div class="privacy-description">
+                <p>
+                    Please also visit our terms and conditions section establishing the use, disclaimers, and limitations of liability governing the use of our website.
+                </p>
+            </div>
+        </p>
+        <br/>
+        <br/>
+        <p>
+            <div class="privacy-title">
+                Your Consent
+            </div>
+            <div class="privacy-description">
+                <p>
+                    By using our site, you consent to our websites privacy policy.
+                </p>
+            </div>
+        </p>
+        <br/>
+        <br/>
+        <p>
+            <div class="privacy-title">
+                Changes to our Privacy Policy
+            </div>
+            <div class="privacy-description">
+                <p>
+                    If we decide to change our privacy policy, we will post those changes on this page.
+                </p>
+            </div>
+        </p>
+        <br>
+        <br>
+    </div>
+</div>
+
+<?php get_footer(); ?>
